77ec852469ee8bee15f150036b917c49486c9220
Maria Sori <mariasori.school@gmail.com>
2021-09-11 10:40:02 -0400

    cloned styles and html

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var generate = function () {
+
+}
+//window alert to select length - 8 < length < 128 characters
+
+// confirm if needs lowercase (y or n)
+
+// confirm if needs upppercase (y or n)
+
+// confirm if needs numeric (y or n)
+
+// confirm if needs special characters (y or n)
