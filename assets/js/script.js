9eee5ba048992c93ded04e163ddfb2347411591d
Maria Sori <mariasori.school@gmail.com>
2021-09-11 21:13:05 -0400

    create variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-var generate = function () {
+//DOM Variables
+var password = document.getElementById('password');
+var generate = document.getElementById('generate');
 
-}
-//window alert to select length - 8 < length < 128 characters
+//generate button
 
-// confirm if needs lowercase (y or n)
+//Character Arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!","@","#","$","%","^","&","*","(",")","+","`","~","-","=","[","]","{","}","|",";",":",",",".","/","<",">","?"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// confirm if needs upppercase (y or n)
+//Variables
+var pwLength = "";
+var confirmLowercase;
+var confirmUppercase;
+var confirmSpecChar;
+var confirmNumber;
 
-// confirm if needs numeric (y or n)
+//Prompt to confirm number of characters
 
-// confirm if needs special characters (y or n)
+    //Loop if 8 > answer > 128
+
+// Password choices
+
+    //loop if one isn't selected
+
+// combining password choices into one array
+
+// generate password function and put into text box
